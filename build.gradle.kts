96b3bf2e31edbb76e3f174bf909f2398b600679e
Aroze <admin@aroze.me>
2024-02-20 22:53:53 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
+plugins {
+    kotlin("jvm") version "1.9.21"
+
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "com.github.uwubans"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+bukkit {
+    name = "UwUBans"
+    version = "${project.version}"
+    authors = listOf("Aroze", "Eva")
+    main = "com.github.uwubans.plugin.UwUBans"
+    apiVersion = "1.20"
+}
---
+++ src/main/kotlin/com/github/uwubans/plugin/UwUBans.kt
@@ -0,0 +1,15 @@
+package com.github.uwubans.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class UwUBans : JavaPlugin() {
+
+    override fun onEnable() {
+        println("hihihiihi :3")
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
