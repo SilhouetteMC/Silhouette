007aa324c21b36e378d70eb98e1c1dc4184c2c94
Aroze <admin@aroze.me>
2024-03-08 14:41:37 +0000

    fix: allow kick without reason

--- src/main/kotlin/com/github/silhouettemc/command/punish/KickCommand.kt
+++ src/main/kotlin/com/github/silhouettemc/command/punish/KickCommand.kt
@@ -6,6 +6,7 @@ import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Description
 import co.aikar.commands.annotation.Flags
+import co.aikar.commands.annotation.Optional
 import co.aikar.commands.bukkit.contexts.OnlinePlayer
 import com.github.silhouettemc.actor.Actor
 import com.github.silhouettemc.punishment.Punishment
@@ -14,20 +15,22 @@ import org.bukkit.entity.Player
 
 @CommandAlias("kick")
 @Description("Kicks a player")
-@CommandPermission("silhouettemc.command.punish.kick")
+@CommandPermission("silhouettemc.punish.kick")
 object KickCommand : BaseCommand() {
 
     @Default
     fun onCommand(
         sender: Player,
         @Flags("other") player: Player,
-        reason: String,
+        @Optional reason: String?,
     ) {
+
         Punishment(
             player.uniqueId,
             Actor(sender.uniqueId),
             reason,
             PunishmentType.KICK,
         ).process()
+
     }
 }
