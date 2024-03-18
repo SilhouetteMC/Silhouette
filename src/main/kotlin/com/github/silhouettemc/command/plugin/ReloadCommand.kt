7a1d6a1abf14cce397e3e4de760877e155a9cd81
Aroze <admin@aroze.me>
2024-03-18 17:49:33 +0000

    feat: subcommand structure for /silhouette

--- src/main/kotlin/com/github/silhouettemc/command/plugin/ReloadCommand.kt
+++ src/main/kotlin/com/github/silhouettemc/command/plugin/ReloadCommand.kt
@@ -5,24 +5,33 @@ import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Description
+import co.aikar.commands.annotation.Subcommand
 import com.github.silhouettemc.util.ConfigUtil
 import com.github.silhouettemc.util.text.send
 import com.github.silhouettemc.util.text.sendTranslated
 import org.bukkit.command.CommandSender
 
-// todo: make this a sub command somehow idk
 @CommandAlias("silhouette")
 @Description("Reloads the Silhouette configuration")
-@CommandPermission("silhouettemc.command.reload")
 object ReloadCommand : BaseCommand() {
-    @Default
-    fun onCommand(
-        sender: CommandSender,
-    ) {
 
+    @Default
+    @Subcommand("info")
+    fun info(sender: CommandSender) {
+        // todo: actual info, also maybe this should be configurable for colors..?
+        sender.sendTranslated("""
+            <p><s>Silhouette</s> info blah blah blah</p>
+            <p>Type <s>/silhouette help</s> for help!
+        """.trimIndent())
+    }
+
+    @Subcommand("reload")
+    @CommandPermission("silhouettemc.command.reload")
+    fun reload(sender: CommandSender) {
         ConfigUtil.load()
         sender.send("reloadConfig")
-
     }
+
+    // todo: subcommands: help, version, info
 
 }
