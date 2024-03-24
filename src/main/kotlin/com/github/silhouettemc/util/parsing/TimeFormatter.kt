36b0dfdd7fd8458bf77f03021f331baf998d65bb
Aroze <admin@aroze.me>
2024-03-24 02:23:58 +0000

    feat: add nanosecond precision to pre-parse actionbar

--- src/main/kotlin/com/github/silhouettemc/util/parsing/TimeFormatter.kt
+++ src/main/kotlin/com/github/silhouettemc/util/parsing/TimeFormatter.kt
@@ -2,18 +2,19 @@ package com.github.silhouettemc.util.parsing
 
 import TimeUnit
 import java.time.Duration
-import java.time.Instant
 
-class TimeFormatter(duration: Duration) {
+class TimeFormatter(val duration: Duration) {
 
     val parsableUnits = TimeUnit.entries.reversed()
 
-    var seconds: Long = duration.seconds
+    var secondsLeft: Long = duration.seconds
+    var nanosRemoved: Long = 0
+
     val sb = StringBuilder()
 
     fun prettify(): String {
         for (unit in parsableUnits) {
-            val removedUnits = attemptToRemove(unit, seconds)
+            val removedUnits = attemptToRemove(unit, secondsLeft)
             if (removedUnits == 1L) sb.append("$removedUnits ${unit.singular.lowercase()}, ")
             if (removedUnits > 1L) sb.append("$removedUnits ${unit.plural.lowercase()}, ")
         }
@@ -28,11 +29,25 @@ class TimeFormatter(duration: Duration) {
         return built
     }
 
-    private fun attemptToRemove(unit: TimeUnit, remaining: Long): Long {
+    private fun attemptToRemove(unit: TimeUnit, remainingSeconds: Long): Long {
         val unitSeconds = unit.duration.seconds
-        if (unitSeconds == 0L) return 0
-        val amount = remaining / unitSeconds
-        seconds -= amount * unitSeconds
+
+        if (unitSeconds == 0L) {
+            val unitNanos = unit.duration.nano
+            if (unitNanos <= 0) return 0 // shouldn't happen!
+
+            val newDuration = duration
+                .minusSeconds(remainingSeconds)
+                .minusNanos(nanosRemoved.toLong())
+
+            val amount = newDuration.nano.toLong() / unitNanos
+            nanosRemoved += amount * unitNanos
+
+            return amount
+        }
+
+        val amount = remainingSeconds / unitSeconds
+        secondsLeft -= amount * unitSeconds
         return amount
     }
 
